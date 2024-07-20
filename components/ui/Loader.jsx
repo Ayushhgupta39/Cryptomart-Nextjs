47e93fc011d38b0b755f2e7e96419a85129049c1
Ayush Gupta <ayushgupta3902@gmail.com>
2024-07-21 00:02:44 +0530

    feat: add files

---
+++ components/ui/Loader.jsx
@@ -0,0 +1,11 @@
+
+
+function Loader() {
+  return (
+    <div className="h-screen flex justify-center w-full items-center">
+      <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin absolute"></div>
+    </div>
+  )
+}
+
+export default Loader
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.coinranking.com',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
